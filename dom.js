4415bc2ed73643ed9d88f114ac1ea69c5b345246
sudh8684 <sudhanshupal234@gmail.com>
2022-11-07 20:01:20 +0530

    comit

---
+++ dom.js
@@ -0,0 +1,7 @@
+let headerTitle=document.getElementById('header-title')
+headerTitle.textContent=('hello')
+let header=document.getElementById('main-header')
+header.style.borderBottom='solid 3px #000'
+let addItems=document.getElementsByClassName('title')
+addItems[0].style.fontWeight='bold'
+addItems[0].style.color='#008000'
