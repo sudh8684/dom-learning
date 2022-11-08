3490b7a98c3f12461ea975b34dfeff64823b283b
sudh8684 <sudhanshupal234@gmail.com>
2022-11-08 12:20:25 +0530

    getelement by tag name

--- dom.js
+++ dom.js
@@ -4,8 +4,11 @@ let header=document.getElementById('main-header')
 header.style.borderBottom='solid 3px #000'
 var addItems=document.getElementsByClassName('list-group-item')
 
-addItems[0].style.fontWeight='bold'
-addItems[1].style.fontWeight='bold'
-addItems[2].style.fontWeight='bold'
-addItems[3].style.fontWeight='bold'
-addItems[2].style.backgroundcolor='#008000'
+//addItems[0].style.fontWeight='bold'
+//addItems[1].style.fontWeight='bold'
+//addItems[2].style.fontWeight='bold'
+//addItems[3].style.fontWeight='bold'
+//addItems[2].style.backgroundcolor='#008000'
+
+let lielement=document.getElementsByTagName('li')
+console.log(lielement)
