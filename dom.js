5ef6e0322cc7afb1a04fe731a24c9ed68d05349f
sudh8684 <sudhanshupal234@gmail.com>
2022-11-08 12:56:14 +0530

    query and query selector all

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-let headerTitle=document.getElementById('header-title')
+/*let headerTitle=document.getElementById('header-title')
 headerTitle.textContent=('hello')
 let header=document.getElementById('main-header')
 header.style.borderBottom='solid 3px #000'
@@ -11,4 +11,14 @@ var addItems=document.getElementsByClassName('list-group-item')
 //addItems[2].style.backgroundcolor='#008000'
 
 let lielement=document.getElementsByTagName('li')
-console.log(lielement)
+console.log(lielement)*/
+
+//query selector &query selector all
+
+let header=document.querySelector('li:nth-child(2)')
+header.style.color='#008000'
+
+let odd=document.querySelectorAll('li:nth-child(odd)')
+for(let i=0;i<odd.length;i++){
+odd[i].style.backgroundColor='green'
+}
